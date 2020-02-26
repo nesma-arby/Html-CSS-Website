0992eccd0888ffc72ec2760990ee662401f76423
nesma-arby <nsmarby@gmail.com>
2020-02-26 15:37:33 +0200

    Native Website

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    
+    
+    $('.nav li a').click(function(){
+        
+        $(this).addClass('active');
+        $('.nav li a').removeClass('active');
+        
+    });
+    
+    
+    
+});
+
+
